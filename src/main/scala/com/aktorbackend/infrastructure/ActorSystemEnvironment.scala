b81417e942ed790afe5d1ef04c7cddb2b8e4fadb
sergeymalyshko <malyshko@actimind.com>
2015-03-28 11:28:48 +0300

    add actors

--- src/main/scala/com/aktorbackend/infrastructure/ActorSystemEnvironment.scala
+++ src/main/scala/com/aktorbackend/infrastructure/ActorSystemEnvironment.scala
@@ -13,13 +13,27 @@ object ActorSystemEnvironment {
   implicit val timeout = Timeout(500, TimeUnit.MILLISECONDS)
   var actorSystem:ActorSystem = null
   val frontName = "frontBalancer"
-  def getPosts(tag:String):Future[Iterable[Post]] = {
-    val frontBalancer:ActorRef =  actorSystem.actorFor(actorSystem /frontName )
-    (frontBalancer ? Tag(tag)).map {l => l.asInstanceOf[Iterable[Post]]}
+  val workerName = "workerBalancer"
+  def createApiWorkerActors(count:Int)={
+    val workers = (for (i <- 0 until count) yield actorSystem.actorOf(Props[ApiWorker]
+      .withDispatcher("dispatchers.proxy-actor-dispatcher"),
+      name=("apiWorker" + i))).toList
+    val internalLoadBalancer = actorSystem.actorOf(
+      Props[ApiWorker].withRouter(RoundRobinRouter(routees = workers)), name = workerName)
+
   }
+
   def init(): Unit = {
     actorSystem = ActorSystem(name = "postfinder", config = ConfigFactory.load.getConfig("postfinder"))
-    val frontBalancer = actorSystem.actorOf(Props[Balancer].withRouter(SmallestMailboxRouter(nrOfInstances = 10)), name=frontName)
+    val worker = createApiWorkerActors(20)
+    val frontBalancer = actorSystem.actorOf(Props[Balancer].withRouter(SmallestMailboxRouter(nrOfInstances = 4)), name=frontName)
+  }
+  def getWorkers:Iterable[ActorRef] = {
+    actorSystem.actorFor(actorSystem /workerName ) :: Nil
+  }
+  def getPosts(tag:String):Future[Iterable[Post]] = {
+    val frontBalancer:ActorRef =  actorSystem.actorFor(actorSystem /frontName )
+    (frontBalancer ? Tag(tag)).mapTo[Iterable[Post]]
   }
 
 
--- src/main/scala/com/aktorbackend/infrastructure/Balancer.scala
+++ src/main/scala/com/aktorbackend/infrastructure/Balancer.scala
@@ -1,15 +1,26 @@
 package com.aktorbackend.infrastructure
 import akka.actor._
 import akka.actor.{ActorRef, Actor}
+import akka.pattern.{ ask, pipe, AskTimeoutException }
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
 
 /**
  * Created by alex on 27.03.15.
  */
 class Balancer extends Actor {
   def receive ={
-    case Tag(name)=>
-      val result = Post("Test Post") :: Nil
-      sender ! result
+    case tag:Tag=>
+      val futures  = ActorSystemEnvironment.getWorkers.map {worker:ActorRef => worker ? tag}
+      val concatinated:Future[List[Post]] =  Future.sequence(futures).mapTo[List[List[Post]]].map{
+        list:List[List[Post]]=>
+          list.foldLeft(List[Post]()){
+            (acc:List[Post],postList:List[Post])=>
+              acc ++ postList
+          }
+      }
+      concatinated.pipeTo(sender)
   }
 
 }
